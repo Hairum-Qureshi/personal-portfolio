8a3af5cba4ab2455bf9855d9bb72b2033e9f8ecd
Hairum Qureshi <hqureshi@udel.edu>
2025-09-06 18:40:32 -0400

    added page for post blog form

--- app/all-blogs/page.tsx
+++ app/all-blogs/page.tsx
@@ -1,3 +1,18 @@
+"use client";
+
+import Link from "next/link";
+
 export default function Blogs() {
-	return <div>All blogs</div>;
+	return (
+		<div>
+			All blogs
+			<br />
+			<Link
+				href="/new-blog"
+				className="text-green-500 underline hover:cursor-pointer"
+			>
+				New Blog
+			</Link>
+		</div>
+	);
 }
---
+++ app/new-blog/page.tsx
@@ -0,0 +1,3 @@
+export default function PostBlog() {
+	return <div>Post blog form</div>;
+}
