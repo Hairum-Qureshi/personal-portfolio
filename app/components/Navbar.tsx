60f27b1a8b49179b0c342bf4fa4495ddf9c4cae5
Hairum Qureshi <hqureshi@udel.edu>
2025-09-06 18:27:12 -0400

    added hover effects to links

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,12 +6,30 @@ export default function Navbar() {
 	return (
 		<nav className="text-white w-full p-2 mt-4">
 			<div className="flex text-lg justify-around text-gray-400">
-				<Link href="/" className="font-semibold">
+				<Link
+					href="/"
+					className="font-semibold hover:text-gray-300 hover:cursor-pointer"
+				>
 					Home
 				</Link>
-				<Link href="/">About</Link>
-				<Link href="/">Project Showcase</Link>
-				<Link href="/">Contact</Link>
+				<Link
+					href="/about"
+					className="hover:text-gray-300 hover:cursor-pointer"
+				>
+					About
+				</Link>
+				<Link
+					href="/project-showcase"
+					className="hover:text-gray-300 hover:cursor-pointer"
+				>
+					Project Showcase
+				</Link>
+				<Link
+					href="/contact"
+					className="hover:text-gray-300 hover:cursor-pointer"
+				>
+					Contact
+				</Link>
 			</div>
 		</nav>
 	);
