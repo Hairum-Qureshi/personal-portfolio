d950400e4aea5c26fd9aa662b5ffcefda3b4192e
Hairum Qureshi <hqureshi@udel.edu>
2025-09-06 18:13:33 -0400

    added Navbar component and Footer component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+	return (
+		<div className="text-gray-400 w-full text-center">
+			© {new Date().getFullYear()} Hairum Qureshi
+		</div>
+	);
+}
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,3 +1,18 @@
+"use client";
+
+import Link from "next/link";
+
 export default function Navbar() {
-	return <nav className="text-white">Navbar</nav>;
+	return (
+		<nav className="text-white w-full p-2 mt-4">
+			<div className="flex text-lg justify-around text-gray-400">
+				<Link href="/" className="font-semibold">
+					Home
+				</Link>
+				<Link href="/">About</Link>
+				<Link href="/">Project Showcase</Link>
+				<Link href="/">Contact</Link>
+			</div>
+		</nav>
+	);
 }
