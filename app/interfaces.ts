631280db4cf4134d28babe074181d5a8bc787e90
Hairum Qureshi <hqureshi@udel.edu>
2025-09-07 14:23:35 -0400

    fixed interfaces' export and added Project interface

--- app/interfaces.ts
+++ app/interfaces.ts
@@ -7,4 +7,12 @@ interface Blog {
 	createdAt: string;
 }
 
-export { type Blog };
+interface Project {
+	project_id: string;
+	project_title: string;
+	project_description: string;
+	project_image: string;
+	project_link: string;
+}
+
+export type { Blog, Project }
