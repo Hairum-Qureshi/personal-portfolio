2389809531d3a15d52edae0190e3aabb3a8a67db
Hairum Qureshi <hqureshi@udel.edu>
2025-09-06 22:21:03 -0400

    fixed issue where footer wasn't at the very bottom of the page regardless of the content

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,12 +12,12 @@ export default function RootLayout({
 			<head>
 				<title>Hairum Qureshi&apos;s Portfolio</title>
 			</head>
-			<body className="relative bg-gray-950 text-gray-300 lg:w-3/5 h-screen flex flex-col m-auto p-2">
+			<body className="bg-gray-950 text-gray-300 lg:w-3/5 min-h-screen flex flex-col m-auto p-2 break-all">
 				<header className="w-full">
 					<Navbar />
 				</header>
-				<main>{children}</main>
-				<footer className="absolute bottom-0 my-2 w-full p-2">
+				<main className="flex-grow">{children}</main>
+				<footer className="w-full p-2 my-5">
 					<Footer />
 				</footer>
 			</body>
