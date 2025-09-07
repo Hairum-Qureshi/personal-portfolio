628423bb3aaa8c941243521884261fd4a4b74426
Hairum Qureshi <hqureshi@udel.edu>
2025-09-07 15:32:08 -0400

    stylized project display div

--- app/project-showcase/page.tsx
+++ app/project-showcase/page.tsx
@@ -1,3 +1,15 @@
+import Project from "../components/Project";
+import projects from "../../public/data/projects.json";
+import { Project as ProjectInterface } from "../interfaces";
+
 export default function ProjectShowcase() {
-	return <div>Project showcase page</div>;
+	return (
+		<div className="grid grid-cols-2 gap-6">
+			{projects.map((project: ProjectInterface) => (
+				<div key={project.project_id} className="hover:cursor-pointer">
+					<Project project={project} />
+				</div>
+			))}
+		</div>
+	);
 }
