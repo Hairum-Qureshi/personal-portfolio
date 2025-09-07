3ad743abc0d7e1591a837d414c060077acf1e15e
Hairum Qureshi <hqureshi@udel.edu>
2025-09-07 00:58:16 -0400

    added text content under 'thank you' message

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,17 @@
 export default function Contact() {
-	return <div>Contact page</div>;
+	return (
+		<div className="h-screen text-white">
+			<div className="w-full h-1/2 text-left flex flex-col items-center justify-center">
+				<h1 className="text-7xl font-semibold text-slate-300">
+					Thanks for checking out <br /> my portfolio!
+				</h1>
+				<p className="m-7 text-gray-400 text-lg">
+					I really appreciate you spending time to view my site. If you&apos;d
+					like to connect, either to collaborate on a project or just to chat,
+					feel free to reach out!
+				</p>
+			</div>
+			<div className="w-full h-1/2 border-2 border-green-500"></div>
+		</div>
+	);
 }
