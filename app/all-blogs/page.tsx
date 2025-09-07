4515f0d60c900226912386f2ae56916c705c4047
Hairum Qureshi <hqureshi@udel.edu>
2025-09-06 23:08:53 -0400

    styled blogs page

--- app/all-blogs/page.tsx
+++ app/all-blogs/page.tsx
@@ -1,18 +1,22 @@
 "use client";
 
 import Link from "next/link";
+import { FaPlus } from "react-icons/fa";
 
 export default function Blogs() {
 	return (
-		<div>
-			All blogs
-			<br />
-			<Link
-				href="/new-blog"
-				className="text-green-500 underline hover:cursor-pointer"
-			>
-				New Blog
-			</Link>
+		<div className="flex">
+			<div>
+				<h1 className="text-2xl font-semibold">All Blogs</h1>
+			</div>
+			<div className="flex items-center justify-center ml-auto">
+				<Link
+					href="/new-blog"
+					className="flex items-center justify-center text-green-500 hover:cursor-pointer p-2 border border-green-400 rounded-md"
+				>
+					<FaPlus />
+				</Link>
+			</div>
 		</div>
 	);
 }
