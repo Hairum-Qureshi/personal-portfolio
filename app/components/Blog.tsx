38d24c42f68bd7e7bd1afcb0d2f6253db34a7169
Hairum Qureshi <hqureshi@udel.edu>
2025-09-07 01:19:56 -0400

    used Moment to format ISO date strings

--- app/components/Blog.tsx
+++ app/components/Blog.tsx
@@ -1,11 +1,15 @@
 import { Blog as BlogInterface } from "../interfaces";
+import moment from "moment";
 
 export default function Blog({ blog }: { blog: BlogInterface }) {
 	return (
-		<div className="my-3 border border-sky-800 rounded-md p-2 hover:cursor-pointer shadow-[0_0_10px_rgba(56,189,248,0.4)] ">
+		<div className="my-5 border border-sky-800 rounded-md p-2 hover:cursor-pointer shadow-[0_0_10px_rgba(56,189,248,0.4)] ">
 			<h2 className="text-xl mb-2 font-semibold">{blog.title}</h2>
 			<p className="text-slate-500">
-				Posted on: <span className="font-semibold">{blog.createdAt}</span>
+				Posted on:{" "}
+				<span className="font-semibold">
+					{moment(blog.createdAt).format("MMMM Do YYYY")}
+				</span>
 			</p>
 			<p className="text-slate-500">Written by: {blog.author}</p>
 			<hr className="my-3 text-sky-700" />
