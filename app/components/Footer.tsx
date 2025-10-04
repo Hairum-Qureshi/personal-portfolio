745e074530aeb4ab14c29c6a192592323485fc31
Hairum Qureshi <hqureshi@udel.edu>
2025-10-04 00:10:24 -0400

    added social media icons to footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,27 @@
+import { FaXTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
+import Link from "next/link";
+
 export default function Footer() {
 	return (
-		<div className="text-gray-400 w-full text-center">
+		<div className="text-gray-400 flex w-full mt-4">
 			© {new Date().getFullYear()} Hairum Qureshi
+			<div className="ml-auto flex items-center space-x-3 text-xl">
+				<Link href="https://x.com/personal_hq">
+					<FaXTwitter />
+				</Link>
+				<Link href="https://www.linkedin.com/in/hairum-qureshi/">
+					<FaLinkedin />
+				</Link>
+				<Link href="https://github.com/hairum-qureshi">
+					<FaGithub />
+				</Link>
+				<Link href="https://instagram.com/personal_hq">
+					<FaInstagram />
+				</Link>
+			</div>
 		</div>
 	);
 }
