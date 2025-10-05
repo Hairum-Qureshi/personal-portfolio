6105114f6be3c80d22989bf64c4fd4060ac40d50
Hairum Qureshi <hqureshi@udel.edu>
2025-10-05 08:25:12 -0400

    made icons change color on hover as well as glow

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -9,16 +9,28 @@ export default function Footer() {
 		<div className="text-gray-400 flex w-full mt-4">
 			© {new Date().getFullYear()} Hairum Qureshi
 			<div className="ml-auto flex items-center space-x-3 text-xl">
-				<Link href="https://x.com/personal_hq">
+				<Link
+					href="https://x.com/personal_hq"
+					className="hover:text-sky-400 hover:drop-shadow-[0_0_6px_#38bdf8] transition duration-300"
+				>
 					<FaXTwitter />
 				</Link>
-				<Link href="https://www.linkedin.com/in/hairum-qureshi/">
+				<Link
+					href="https://www.linkedin.com/in/hairum-qureshi/"
+					className="hover:text-sky-400 hover:drop-shadow-[0_0_6px_#38bdf8] transition duration-300"
+				>
 					<FaLinkedin />
 				</Link>
-				<Link href="https://github.com/hairum-qureshi">
+				<Link
+					href="https://github.com/hairum-qureshi"
+					className="hover:text-purple-400 hover:drop-shadow-[0_0_6px_#a855f7] transition duration-300"
+				>
 					<FaGithub />
 				</Link>
-				<Link href="https://instagram.com/personal_hq">
+				<Link
+					href="https://instagram.com/personal_hq"
+					className="hover:text-fuchsia-400 hover:drop-shadow-[0_0_6px_#e879f9] transition duration-300"
+				>
 					<FaInstagram />
 				</Link>
 			</div>
