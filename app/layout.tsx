6a17e67f1cbd44d357e3bd7a15cf44de7fb0c59d
Hairum Qureshi <hqureshi@udel.edu>
2025-10-05 07:46:47 -0400

    changed break-words to break-normal

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,12 +12,12 @@ export default function RootLayout({
 			<head>
 				<title>Hairum Qureshi&apos;s Portfolio</title>
 			</head>
-			<body className="bg-gray-950 text-gray-300 lg:w-3/5 min-h-screen flex flex-col m-auto p-2 break-all">
+			<body className="bg-gray-950 text-gray-300 lg:w-3/5 min-h-screen flex flex-col m-auto p-2 break-normal">
 				<header className="w-full">
 					<Navbar />
 				</header>
 				<main className="flex-grow">{children}</main>
-				<footer className="w-full p-2 my-5">
+				<footer className="w-full p-2 mt-10 mb-5">
 					<Footer />
 				</footer>
 			</body>
