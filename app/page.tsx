d8e2fd432e0c1c980714512ed5490e20da3b9354
Hairum Qureshi <hqureshi@udel.edu>
2025-10-05 08:18:52 -0400

    updated photo dimensions

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,13 @@ export default function Home() {
 			<div className="mt-3 flex">
 				<Image
 					src={self}
-					width={250}
+					width={150}
+					height={150}
 					alt="Picture of the author"
-					className="rounded-md opacity-80 border-2 border-gray-600"
+					className="rounded-md opacity-80 border-2 border-sky-800 object-cover"
 				/>
 
-				<h1 className="text-5xl font-bold m-3">
+				<h1 className="text-5xl font-bold ml-3">
 					Hello, I&apos;m Hairum Qureshi
 				</h1>
 			</div>
