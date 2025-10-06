0f1a270a1766a3abdf6136b79aab2f7b9f6877ac
Hairum Qureshi <hqureshi@udel.edu>
2025-10-06 18:39:44 -0400

    styled header, updated color schemes, and info

--- app/page.tsx
+++ app/page.tsx
@@ -19,10 +19,14 @@ export default function Home() {
 	return (
 		<div className="lg:mx-0 mx-5">
 			<div className="mt-3">
-				<h2 className="font-semibold text-3xl lg:px-0 px-3">Hello, I&apos;m</h2>
-				<h1 className="font-semibold text-yellow-500 lg:text-8xl text-7xl lg:p-0 px-3">
+				<h2 className="text-3xl lg:px-0 px-3">Hello, I&apos;m</h2>
+				<h1 className="font-semibold text-indigo-500 lg:text-8xl text-7xl lg:p-0 px-3 drop-shadow-[0_0_40px_rgba(155,93,229,0.9)]">
 					Hairum Qureshi
 				</h1>
+				<h3 className="text-xl mt-1">
+					An emerging full-stack software engineer focused on clean, intuitive
+					design
+				</h3>
 				<div className="py-8">
 					<h3 className="mb-4 text-white text-xl font-bold tracking-tight">
 						Quick Facts:
@@ -32,12 +36,12 @@ export default function Home() {
 							<div className="flex flex-col items-start space-y-1">
 								<div className="flex items-center space-x-2">
 									<FaUser className="text-2xl text-white/70" />
-									<span className="text-4xl font-extrabold text-sky-400">
+									<span className="text-4xl font-extrabold text-yellow-500">
 										{new Date().getFullYear() - 2004}
 									</span>
 								</div>
 								<span className="text-sm font-medium text-gray-300/80 mt-[-5px]">
-									Age
+									Years old
 								</span>
 							</div>
 						</div>
@@ -45,7 +49,7 @@ export default function Home() {
 							<div className="flex flex-col items-start space-y-1">
 								<div className="flex items-center space-x-2">
 									<FaLaptopCode className="text-3xl text-white/70" />
-									<span className="text-4xl font-extrabold text-sky-400">
+									<span className="text-4xl font-extrabold text-yellow-500">
 										{new Date().getFullYear() - 2019}
 									</span>
 								</div>
@@ -58,12 +62,15 @@ export default function Home() {
 							<div className="flex flex-col items-start space-y-1">
 								<div className="flex items-center space-x-2">
 									<IoLocation className="text-2xl text-white/70" />
-									<span className="text-4xl font-semibold text-sky-400">
+									<span className="text-4xl font-semibold text-yellow-500">
+										{/* <span className="text-red-500">U</span>
+										<span className="text-white">S</span>
+										<span className="text-sky-500">A</span> */}
 										USA
 									</span>
 								</div>
 								<span className="text-sm font-medium text-gray-300/80 mt-[-5px]">
-									Delaware
+									Newark, Delaware
 								</span>
 							</div>
 						</div>
@@ -71,7 +78,7 @@ export default function Home() {
 							<div className="flex flex-col items-start space-y-1">
 								<div className="flex items-center space-x-2">
 									<SiNextdotjs className="text-2xl text-white/70" />
-									<span className="text-4xl font-semibold text-sky-400">
+									<span className="text-4xl font-semibold text-sky-500">
 										Next.js
 									</span>
 								</div>
