67882526e95315fa09123b02f6b276de32adaa98
Hairum Qureshi <hqureshi@udel.edu>
2025-10-06 01:49:57 -0400

    added code to prevent rendering a project that has missing data

--- app/project-showcase/page.tsx
+++ app/project-showcase/page.tsx
@@ -5,14 +5,17 @@ import { Project as ProjectInterface } from "../interfaces";
 export default function ProjectShowcase() {
 	return (
 		<div className="lg:grid lg:grid-cols-2 lg:gap-6">
-			{projects.map((project: ProjectInterface) => (
-				<div
-					key={project.project_id}
-					className="hover:cursor-pointer mx-2 lg:mx-0"
-				>
-					<Project project={project} />
-				</div>
-			))}
+			{projects.map(
+				(project: ProjectInterface) =>
+					project.project_github_link && (
+						<div
+							key={project.project_id}
+							className="hover:cursor-pointer mx-2 lg:mx-0"
+						>
+							<Project project={project} />
+						</div>
+					)
+			)}
 		</div>
 	);
 }
