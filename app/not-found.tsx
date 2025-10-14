ab095ad8177e4bb0d769fecfedeac4f2be94f4e7
Hairum Qureshi <hqureshi@udel.edu>
2025-10-13 22:21:11 -0400

    added custom 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+	return (
+		<div className="w-full flex items-center justify-center mt-60">
+			<div className="flex items-center">
+				<h1 className="text-3xl">404</h1>
+				<span className="border-r-2 h-7 border-white mx-5"></span>
+				<h1 className="text-3xl">Page Not Found</h1>
+			</div>
+		</div>
+	);
+}
